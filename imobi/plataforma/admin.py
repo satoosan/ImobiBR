44b9f9e65d66a286baaa3fcaa905cda4be007120
not_a_number <satogui5@gmail.com>
2022-01-16 21:18:55 -0300

    Imobi - Agendamento de Imoveis

---
+++ imobi/plataforma/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import DiasVisita, Cidade, Imagem, Horario, Imovei, Visitas
+
+
+@admin.register(Imovei)
+class ImoveiAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
+    list_editable = ('valor', 'tipo')
+    list_filter = ('cidade', 'tipo')
+
+
+# Register your models here.
+admin.site.register(DiasVisita)
+admin.site.register(Cidade)
+admin.site.register(Imagem)
+admin.site.register(Horario)
+admin.site.register(Visitas)
